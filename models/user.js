1ebb29f888d486044295bbbb0e9e31ea5960c82f
Godswill Utong <godswillutong@gmail.com>
2023-10-03 18:37:53 +0100

    first commit

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+  username: { type: String },
+  password: { type: String, unique: true },
+  email: { type: String, unique: true },
+  phone_number: { type: String },
+  gender: { type: String, value: ["male", "female"] },
+  address: { type: String },
+  city: { type: String },
+  state: { type: String },
+  country: { type: String },
+});
+
+UserSchema.pre("save", async function (next) {
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+  next();
+});
+
+UserSchema.methods.isValidPassword = async function (password) {
+  const user = this;
+  const compare = await bcrypt.compare(password, user.password);
+  return compare;
+};
+
+module.exports = mongoose.model("users", UserSchema);
